2a50f3c29724ba711d079409b8e4d32111e27910
Ronald <ronaldrrca@gmail.com>
2021-07-05 11:39:55 -0500

    ajustes de ortografía en la sección de benefits

--- enviar_mail.php
+++ enviar_mail.php
@@ -1,26 +1,32 @@
 <?php
-$nombre = $_POST['nombre'];
+$name = $_POST['name'];
 $cellphone = $_POST['cellphone'];
 $phone = $_POST['phone'];
 $mail = $_POST['email'];
 $city = $_POST['city'];
+
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+$message = "Este mensaje fue enviado por " . $name . ",\r\n";
+$message .= "Teléfono celular: " . $cellphone . " \r\n";
+$message .= "Teléfono fijo: " . $phone . " \r\n";
+$message .= "E-mail: " . $mail . " \r\n";
+$message .= "Ciudad: " . $city . " \r\n";
+$message .= "Enviado el " . date('d/m/Y', time());
+// $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 
-$para = 'ignaciobluuweb@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+$to = 'bongoyor@gmail.com';
+$subject = 'Mensaje de mi sitio web';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+mail($to, $subject, utf8_decode($message), $header);
 
-header("Location:index.html");
+// header("Location:index.html");
 
 //Revisar bien los datos antes de la entrega
+
+
 ?>
