709a861f0ba4c6b1e35b8c482aeadaa10108e1da
Ronald <ronaldrrca@gmail.com>
2021-07-07 18:40:06 -0500

    ajuste del titulo, colores del formulario y ocultando el icono de twitter

--- enviar_mail.php
+++ enviar_mail.php
@@ -1,10 +1,10 @@
 <?php
 $name = $_POST['name'];
 $cellphone = $_POST['cellphone'];
-$phone = $_POST['phone'];
 $mail = $_POST['email'];
-$city = $_POST['city'];
-
+$cityCountry = $_POST['cityCountry'];
+$type = $_POST['type'];
+$text = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,18 +13,19 @@ $header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por " . $name . ",\r\n";
 $message .= "Teléfono celular: " . $cellphone . " \r\n";
-$message .= "Teléfono fijo: " . $phone . " \r\n";
 $message .= "E-mail: " . $mail . " \r\n";
-$message .= "Ciudad: " . $city . " \r\n";
+$message .= "Ciudad y país: " . $cityCountry . " \r\n";
+$message .= "Tipo de solicitud: " . $type . " \r\n";
+$message .= "Mensaje: " . $text . " \r\n";
 $message .= "Enviado el " . date('d/m/Y', time());
-// $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+
 
 $to = 'bongoyor@gmail.com';
 $subject = 'Mensaje de mi sitio web';
 
 mail($to, $subject, utf8_decode($message), $header);
 
-// header("Location:index.html");
+header("Location:index.html");
 
 
 
